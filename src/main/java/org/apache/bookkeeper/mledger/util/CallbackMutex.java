2d824c21b6d336fd65fe641ea47503478c43536b
mmerli <mmerli@3b44db21-3a6d-4d5f-afd4-25f0118656d7>
2013-01-02 23:34:04 +0000

    Refactored CallbackMutex to use Semaphore
    git-svn-id: svn+ssh://svn.corp.yahoo.com/yahoo/platform/cloud_messaging/trunk@1428 3b44db21-3a6d-4d5f-afd4-25f0118656d7

--- src/main/java/org/apache/bookkeeper/mledger/util/CallbackMutex.java
+++ src/main/java/org/apache/bookkeeper/mledger/util/CallbackMutex.java
@@ -13,57 +13,46 @@
  */
 package org.apache.bookkeeper.mledger.util;
 
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.locks.LockSupport;
+import static org.apache.bookkeeper.mledger.util.VarArgs.va;
+
+import java.util.concurrent.Semaphore;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Mutex object that can be acquired from a thread and released from a different
- * thread.
+ * Mutex object that can be acquired from a thread and released from a different thread.
  * 
- * This is meant to be acquired when calling an asynchronous method and released
- * in its callback which is probably executed in a different thread.
+ * This is meant to be acquired when calling an asynchronous method and released in its callback which is probably
+ * executed in a different thread.
  */
 public class CallbackMutex {
 
-    private AtomicBoolean locked = new AtomicBoolean(false);
-    private Queue<Thread> waiters = new ConcurrentLinkedQueue<Thread>();
+    private Semaphore semaphore = new Semaphore(1, true);
+
     String owner;
     String position;
 
     public void lock() {
-        boolean wasInterrupted = false;
-        Thread current = Thread.currentThread();
-        waiters.add(current);
-
-        // Block while not first in queue or cannot acquire lock
-        while (waiters.peek() != current && !locked.compareAndSet(false, true)) {
-            LockSupport.park();
-            if (Thread.interrupted()) // ignore interrupts while waiting
-                wasInterrupted = true;
-        }
-
-        waiters.remove();
-        if (wasInterrupted) { // reassert interrupt status on exit
-            current.interrupt();
-        }
+        semaphore.acquireUninterruptibly();
 
         owner = Thread.currentThread().getName();
-        position = Thread.currentThread().getStackTrace()[2].toString();
-        log.debug("<<< Lock {} acquired at {}", this, position);
+
+        if (log.isDebugEnabled()) {
+            position = Thread.currentThread().getStackTrace()[2].toString();
+            log.debug("<<< Lock {} acquired at {}", va(this.hashCode(), position));
+        }
     }
 
     public void unlock() {
         owner = null;
         position = null;
-        log.debug(">>> Lock {} released at {}", this, Thread.currentThread().getStackTrace()[2]);
+        if (log.isDebugEnabled()) {
+            log.debug(">>> Lock {} released token={} at {}",
+                    va(this.hashCode(), Thread.currentThread().getStackTrace()[2]));
+        }
 
-        locked.set(false);
-        LockSupport.unpark(waiters.peek());
+        semaphore.release();
     }
 
     private static final Logger log = LoggerFactory.getLogger(CallbackMutex.class);
