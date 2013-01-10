d6a7afd4948166c64aac50a190ff56dfc1b0beb4
mmerli <mmerli@3b44db21-3a6d-4d5f-afd4-25f0118656d7>
2013-01-10 00:38:38 +0000

    Added CallbackMutexReadWrite debug logs
    git-svn-id: svn+ssh://svn.corp.yahoo.com/yahoo/platform/cloud_messaging/trunk@1463 3b44db21-3a6d-4d5f-afd4-25f0118656d7

--- src/main/java/org/apache/bookkeeper/mledger/util/CallbackMutexReadWrite.java
+++ src/main/java/org/apache/bookkeeper/mledger/util/CallbackMutexReadWrite.java
@@ -13,10 +13,17 @@
  */
 package org.apache.bookkeeper.mledger.util;
 
+import java.util.Map;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Maps;
 
 /**
  * Read/Write Mutex that can be lock from one thread and released from a different one. There can be multiple readers
@@ -26,40 +33,65 @@ public class CallbackMutexReadWrite {
     private final Lock lock = new ReentrantLock(true);
     private final Condition condition = lock.newCondition();
     private final Semaphore semaphore = new Semaphore(1, true);
-    private int readers = 0;
+    private AtomicInteger readers = new AtomicInteger(0);
+
+    private String writeThread;
+    private String writeThreadPosition;
+    private Map<String, String> readerThreads = Maps.newHashMap();
 
     public void lockRead() {
+        log.debug("lockRead entering");
         semaphore.acquireUninterruptibly();
         lock.lock();
-        ++readers;
+        int r = readers.incrementAndGet();
+
+        String threadName = Thread.currentThread().getName();
+        String threadPosition = Thread.currentThread().getStackTrace()[2].toString();
+        readerThreads.put(threadName, threadPosition);
+
         lock.unlock();
         semaphore.release();
+        log.debug("lockRead got lock readers={} at {} ", r, threadPosition);
     }
 
     public void unlockRead() {
+        log.debug("unlockRead entering");
         lock.lock();
-        readers -= 1;
-        if (readers == 0) {
-            condition.signal();
+        int r = readers.decrementAndGet();
+        if (r == 0) {
+            log.debug("unlockRead signal writer");
+            condition.signalAll();
         }
 
         lock.unlock();
+        log.debug("unlockRead released --- readers={}", r);
     }
 
     public void lockWrite() {
+        log.debug("lockWrite entering");
         semaphore.acquireUninterruptibly();
         lock.lock();
 
-        if (readers > 0) {
+        writeThread = Thread.currentThread().getName();
+        writeThreadPosition = Thread.currentThread().getStackTrace()[2].toString();
+
+        if (readers.get() > 0) {
             // Wait until all the readers are done.
+            log.debug("lockWrite wait for {} readers", readers);
             condition.awaitUninterruptibly();
         }
 
         lock.unlock();
+        log.debug("<<<<<<<<  lockWrite got lock at {}", writeThreadPosition);
     }
 
     public void unlockWrite() {
+        writeThread = null;
+        writeThreadPosition = null;
+
+        log.debug(">>>> unlockWrite");
         semaphore.release();
     }
 
+    private static final Logger log = LoggerFactory.getLogger(CallbackMutexReadWrite.class);
 }
